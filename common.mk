0b6cfb0bf54b255862bbd5e72938bdd7780a616b
Michael Bestas <mkbestas@lineageos.org>
2026-05-05 21:44:38 +0200

    sm6375-common: Convert Aperture overlay to RRO
    Change-Id: If0abfcbc0a0c228dfe0cb3b4cd2f8b2fca7e8525

--- common.mk
+++ common.mk
@@ -32,6 +32,7 @@ PRODUCT_PACKAGES += \
     FrameworksResCommon \
     FrameworksResProduct \
     FrameworksResTarget \
+    LineageApertureAppCommon \
     NcmTetheringOverlay \
     SettingsProviderResCommon \
     SystemUIResCommon \
